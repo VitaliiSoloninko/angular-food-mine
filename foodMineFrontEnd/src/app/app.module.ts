ee4addd5c06419013d39082e67c441d70bd97633
22051988Di <vitalii.soloninko@gmail.com>
2024-05-30 11:13:30 +0200

    v0.1.5	Create Home component ts

--- foodMineFrontEnd/src/app/app.module.ts
+++ foodMineFrontEnd/src/app/app.module.ts
@@ -7,9 +7,10 @@ import {
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
---
+++ foodMineFrontEnd/src/app/components/pages/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../../../services/food.service';
+import { Food } from '../../../shared/models/Food';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent implements OnInit {
+  foods: Food[] = [];
+  constructor(private foodService: FoodService) {
+    this.foods = foodService.getAll();
+  }
+
+  ngOnInit(): void {}
+}
