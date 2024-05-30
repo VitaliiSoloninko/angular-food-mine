1906c36b1b078670f9edcc0ad2c6ff3bc787ba7d
22051988Di <vitalii.soloninko@gmail.com>
2024-05-30 16:27:38 +0200

    v0.1.6	Create Home component html

--- foodMineFrontEnd/src/app/app.module.ts
+++ foodMineFrontEnd/src/app/app.module.ts
@@ -6,8 +6,8 @@ import {
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/partials/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { HeaderComponent } from './components/partials/header/header.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, HomeComponent],
