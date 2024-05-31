ce990e655de98d7ff41d43df72af5616f9d39777
22051988Di <vitalii.soloninko@gmail.com>
2024-05-31 10:40:19 +0200

    v0.2.1	Add method to Food service

--- foodMineFrontEnd/src/app/services/food.service.ts
+++ foodMineFrontEnd/src/app/services/food.service.ts
@@ -11,4 +11,11 @@ export class FoodService {
   getAll(): Food[] {
     return sample_foods;
   }
+
+  getAllFoodBySearchTerm(searchTerm: string) {
+    // Pizza pizza
+    return this.getAll().filter((food) =>
+      food.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }
 }
