6ed30107069dce480158d98a2182e04d43308fca
22051988Di <vitalii.soloninko@gmail.com>
2024-05-31 10:58:05 +0200

    v0.2.4	Search component add to home component

--- foodMineFrontEnd/src/app/app.module.ts
+++ foodMineFrontEnd/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { HeaderComponent } from './components/partials/header/header.component';
+import { SearchComponent } from './components/partials/search/search.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
