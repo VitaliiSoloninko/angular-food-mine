4d9d3f3e2d0a269e8580714b69b493dd45b9972b
22051988Di <vitalii.soloninko@gmail.com>
2024-05-31 12:22:11 +0200

    v0.2.7	Search component css

--- foodMineFrontEnd/src/app/components/partials/search/search.component.ts
+++ foodMineFrontEnd/src/app/components/partials/search/search.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrl: './search.component.css',
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  search(arg0: string) {
+    throw new Error('Method not implemented.');
+  }
+  // searchTerm = '';
 
+  // constructor(activatedRoute: ActivatedRoute, private router: Router) {
+  //   activatedRoute.params.subscribe((params) => {
+  //     if (params.searchTerm) this.searchTerm = params.searchTerm;
+  //   });
+  // }
+
+  ngOnInit(): void {}
+
+  // search(term: string): void {
+  //    if (term) this.router.navigateByUrl('/search/' + term);
+  // }
 }
