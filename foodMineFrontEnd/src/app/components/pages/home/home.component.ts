e3d5ada6ee3488ffbd5c6ff05d3603e5a9cee3eb
22051988Di <vitalii.soloninko@gmail.com>
2024-05-31 10:53:38 +0200

    v0.2.3	Show search result in Home component,  "noPropertyAccessFromIndexSignature": false, http://localhost:4200/search/pizza

--- foodMineFrontEnd/src/app/components/pages/home/home.component.ts
+++ foodMineFrontEnd/src/app/components/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { FoodService } from '../../../services/food.service';
 import { Food } from '../../../shared/models/Food';
 
@@ -9,8 +10,15 @@ import { Food } from '../../../shared/models/Food';
 })
 export class HomeComponent implements OnInit {
   foods: Food[] = [];
-  constructor(private foodService: FoodService) {
-    this.foods = foodService.getAll();
+  constructor(
+    private foodService: FoodService,
+    activatedRoute: ActivatedRoute
+  ) {
+    activatedRoute.params.subscribe((params) => {
+      if (params.searchTerm)
+        this.foods = this.foodService.getAllFoodBySearchTerm(params.searchTerm);
+      else this.foods = foodService.getAll();
+    });
   }
 
   ngOnInit(): void {}
