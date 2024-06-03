0dc3bbcdd3b278bf8d32151823e36300b769fa5a
22051988Di <vitalii.soloninko@gmail.com>
2024-06-03 16:12:13 +0200

    v0.4.2	Generate Food Page component and Add Route

--- foodMineFrontEnd/src/app/app.module.ts
+++ foodMineFrontEnd/src/app/app.module.ts
@@ -10,9 +10,10 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { HeaderComponent } from './components/partials/header/header.component';
 import { SearchComponent } from './components/partials/search/search.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
+import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, TagsComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, TagsComponent, FoodPageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
