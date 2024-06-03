14bb860bc3214d6578796f76265af8a39c8fb894
22051988Di <vitalii.soloninko@gmail.com>
2024-06-03 15:13:18 +0200

    v0.3.7	Add to home component ts html css

--- foodMineFrontEnd/src/app/components/partials/tags/tags.component.ts
+++ foodMineFrontEnd/src/app/components/partials/tags/tags.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Tag } from '../../../shared/models/Tag';
+import { FoodService } from '../../../services/food.service';
 
 @Component({
   selector: 'app-tags',
   templateUrl: './tags.component.html',
-  styleUrl: './tags.component.css'
+  styleUrl: './tags.component.css',
 })
-export class TagsComponent {
-
+export class TagsComponent implements OnInit {
+  tags?: Tag[];
+  constructor(foodService: FoodService) {
+    this.tags = foodService.getAllTags();
+  }
+  ngOnInit(): void {}
 }
