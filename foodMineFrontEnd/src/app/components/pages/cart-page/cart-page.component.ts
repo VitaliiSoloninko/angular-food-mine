d29854826db2a45c5c2e84c10b071b56cf7e0f53
22051988Di <vitalii.soloninko@gmail.com>
2024-06-07 13:22:27 +0200

    v0.5.7	Cart page - Add ts

--- foodMineFrontEnd/src/app/components/pages/cart-page/cart-page.component.ts
+++ foodMineFrontEnd/src/app/components/pages/cart-page/cart-page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../../services/cart.service';
+import { Cart } from '../../../shared/models/Cart';
+import { CartItem } from '../../../shared/models/CartItem';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
-  styleUrl: './cart-page.component.css'
+  styleUrl: './cart-page.component.css',
 })
-export class CartPageComponent {
+export class CartPageComponent implements OnInit {
+  cart!: Cart;
 
+  constructor(private cartService: CartService) {
+    this.cartService.getCartObservable().subscribe((cart) => {
+      this.cart = cart;
+    });
+  }
+  ngOnInit(): void {}
+
+  removeFromCart(cartItem: CartItem) {
+    this.cartService.removeFromCart(cartItem.food.id);
+  }
+
+  changeQuantity(cartItem: CartItem, quantityInString: string) {
+    const quantity = parseInt(quantityInString);
+    this.cartService.changeQuantity(cartItem.food.id, quantity);
+  }
 }
