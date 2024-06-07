b73b31835445e6f652d540cad6ebac3dfb5a3275
22051988Di <vitalii.soloninko@gmail.com>
2024-06-07 11:05:37 +0200

    v0.5.5	Food Page - Add to Cart Button method addToCart

--- foodMineFrontEnd/src/app/components/pages/food-page/food-page.component.ts
+++ foodMineFrontEnd/src/app/components/pages/food-page/food-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Food } from '../../../shared/models/Food';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CartService } from '../../../services/cart.service';
 import { FoodService } from '../../../services/food.service';
+import { Food } from '../../../shared/models/Food';
 
 @Component({
   selector: 'app-food-page',
@@ -11,11 +12,21 @@ import { FoodService } from '../../../services/food.service';
 export class FoodPageComponent implements OnInit {
   food!: Food;
 
-  constructor(activatedRoute: ActivatedRoute, foodService: FoodService) {
+  constructor(
+    activatedRoute: ActivatedRoute,
+    foodService: FoodService,
+    private cartService: CartService,
+    private router: Router
+  ) {
     activatedRoute.params.subscribe((params) => {
       if (params.id) this.food = foodService.getFoodById(params.id);
     });
   }
 
   ngOnInit(): void {}
+
+  addToCart() {
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
+  }
 }
