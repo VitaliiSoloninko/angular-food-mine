0b54df2a4dfa31d805e2d8fee6fa471d9a6c8e38
22051988Di <vitalii.soloninko@gmail.com>
2024-06-10 16:09:28 +0200

    v0.7.5	App get

--- foodMineBackEnd/src/server.ts
+++ foodMineBackEnd/src/server.ts
@@ -1,5 +1,8 @@
 import express from 'express'
 import cors from 'cors'
+// cors for redirect localhost server
+// localhost: 4200 - Angular
+// localhost: 5000 - Express
 
 const app = express()
 app.use(
@@ -9,5 +12,11 @@ app.use(
 	})
 )
 
-// localhost: 4200 - Angular
-// localhost: 5000 - Express
+app.get('/api/foods', (req, res) => {
+	res.send('hello world!')
+})
+
+const port = 5000
+app.listen(port, () => {
+	console.log('Website served on http://localhost: ' + port)
+})
