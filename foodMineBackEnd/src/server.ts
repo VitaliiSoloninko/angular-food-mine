96908af97a8e97ba3a8f8460a7db6efa7500041b
22051988Di <vitalii.soloninko@gmail.com>
2024-06-10 16:50:17 +0200

    v0.7.7	All 5 foods api in server.ts

--- foodMineBackEnd/src/server.ts
+++ foodMineBackEnd/src/server.ts
@@ -1,5 +1,6 @@
-import express from 'express'
 import cors from 'cors'
+import express from 'express'
+import { sample_foods, sample_tags } from './data'
 // cors for redirect localhost server
 // localhost: 4200 - Angular
 // localhost: 5000 - Express
@@ -13,7 +14,31 @@ app.use(
 )
 
 app.get('/api/foods', (req, res) => {
-	res.send('hello world!')
+	res.send(sample_foods)
+})
+
+app.get('/api/foods/search/:searchTerm', (req, res) => {
+	const searchTerm = req.params.searchTerm
+	const foods = sample_foods.filter(food =>
+		food.name.toLowerCase().includes(searchTerm.toLowerCase())
+	)
+	res.send(foods)
+})
+
+app.get('/api/foods/tags', (req, res) => {
+	res.send(sample_tags)
+})
+
+app.get('/api/foods/tag/:tagName', (req, res) => {
+	const tagName = req.params.tagName
+	const foods = sample_foods.filter(food => food.tags?.includes(tagName))
+	res.send(foods)
+})
+
+app.get('/api/foods/:foodId', (req, res) => {
+	const foodId = req.params.foodId
+	const food = sample_foods.find(food => food.id == foodId)
+	res.send(food)
 })
 
 const port = 5000
