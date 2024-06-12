c42feb8e6e445f62df37c1b5f35487cb0b9796f4
22051988Di <vitalii.soloninko@gmail.com>
2024-06-12 12:35:41 +0200

    v0.9.2	Add User Subject, Add User Urls, IUserLogin interface

--- foodMineFrontEnd/src/app/services/user.service.ts
+++ foodMineFrontEnd/src/app/services/user.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../shared/models/User';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  private userSubject = new BehaviorSubject<User>(new User());
+  public userObservable: Observable<User>;
+  constructor(private http: HttpClient) {
+    this.userObservable = this.userSubject.asObservable();
+  }
 
-  constructor() { }
+  // login(userLogin: IUserLogin): Observable<User> {
+  //   return this.http
+  //     .post<User>(USER_LOGIN_URL, userLogin)
+  //     .pipe(tap({ next: (user) => {} }, error: (errorResponse)=>{}));
+  // }
 }
