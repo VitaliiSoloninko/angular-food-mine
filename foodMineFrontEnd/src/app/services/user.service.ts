0f806d9c0974a3ce15230d6f159fc5ad0eb23aad
22051988Di <vitalii.soloninko@gmail.com>
2024-06-12 14:56:41 +0200

    v0.9.6	User Service Local Storage

--- foodMineFrontEnd/src/app/services/user.service.ts
+++ foodMineFrontEnd/src/app/services/user.service.ts
@@ -6,11 +6,14 @@ import { IUserLogin } from '../shared/interfaces/IUserLogin';
 import { User } from '../shared/models/User';
 import { USER_LOGIN_URL } from '../shared/models/constants/urls';
 
+const USER_KEY = 'User';
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private userSubject = new BehaviorSubject<User>(new User());
+  private userSubject = new BehaviorSubject<User>(
+    this.getUserFromLocalStorage()
+  );
   public userObservable: Observable<User>;
   constructor(private http: HttpClient, private toastrService: ToastrService) {
     this.userObservable = this.userSubject.asObservable();
@@ -20,6 +23,7 @@ export class UserService {
     return this.http.post<User>(USER_LOGIN_URL, userLogin).pipe(
       tap({
         next: (user) => {
+          this.setUserToLocalStorage(user);
           this.userSubject.next(user);
           this.toastrService.success(
             `Welcome to Foodmine ${user.name}!`,
@@ -31,5 +35,15 @@ export class UserService {
         },
       })
     );
+  }
+
+  private setUserToLocalStorage(user: User) {
+    localStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+
+  private getUserFromLocalStorage(): User {
+    const userJson = localStorage.getItem(USER_KEY);
+    if (userJson) return JSON.parse(userJson) as User;
+    return new User();
   }
 }
