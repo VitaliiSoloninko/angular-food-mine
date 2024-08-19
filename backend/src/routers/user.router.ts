20dce914baf21dc577cab9d2dfa594eb1a722e8e
22051988Di <vitalii.soloninko@gmail.com>
2024-08-19 21:45:39 +0200

    v0.12.1	Register Page: Create register api

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -2,7 +2,9 @@ import Router from 'express'
 import expressAsyncHandler from 'express-async-handler'
 import jwt from 'jsonwebtoken'
 import { sample_users } from '../data'
-import { UserModel } from '../models/user.model'
+import { User, UserModel } from '../models/user.model'
+import { HTTP_BAD_REQUEST } from '../constants/http_status'
+import bcrypt from 'bcryptjs'
 
 const router = Router()
 
@@ -24,12 +26,37 @@ router.post(
 	expressAsyncHandler(async (req, res) => {
 		const { email, password } = req.body // Destructuring Assignment
 		const user = await UserModel.findOne({ email, password })
-
 		if (user) {
 			res.send(generateTokenResponse(user))
 		} else {
-			res.status(400).send('User name or password is not valid!')
+			res.status(HTTP_BAD_REQUEST).send('User name or password is not valid!')
 		}
+	})
+)
+
+router.post(
+	'/register',
+	expressAsyncHandler(async (req, res) => {
+		const { name, email, password, address } = req.body
+		const user = await UserModel.findOne({ email })
+		if (user) {
+			res.status(HTTP_BAD_REQUEST).send('User is already exist, please login')
+			return
+		}
+
+		const encryptedPassword = await bcrypt.hash(password, 10)
+
+		const newUser: User = {
+			id: '',
+			name,
+			email: email.toLowerCase(),
+			password: encryptedPassword,
+			address,
+			isAdmin: false,
+		}
+
+		const dbUser = await UserModel.create(newUser)
+		res.send(generateTokenResponse(dbUser))
 	})
 )
 
