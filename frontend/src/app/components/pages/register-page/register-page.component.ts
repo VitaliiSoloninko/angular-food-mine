1cf4cbc45e926cfc511e55d9cc5641ce9741b228
22051988Di <vitalii.soloninko@gmail.com>
2024-08-20 14:54:19 +0200

    v0.12.5	Register component Form Validators

--- frontend/src/app/components/pages/register-page/register-page.component.ts
+++ frontend/src/app/components/pages/register-page/register-page.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from 'express';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styleUrl: './register-page.component.css'
+  styleUrl: './register-page.component.css',
 })
-export class RegisterPageComponent {
+export class RegisterPageComponent implements OnInit {
+  registerForm!: FormGroup;
+  isSubmitted = false;
 
+  returnUrl = '';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(5)]],
+      email: ['', [Validators.required], [Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+      confirmPassword: ['', [Validators.required]],
+      address: ['', [Validators.required, Validators.minLength(10)]],
+    });
+  }
 }
