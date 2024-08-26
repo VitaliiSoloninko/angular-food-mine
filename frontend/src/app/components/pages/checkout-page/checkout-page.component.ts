f1d2ce11ea27f20cbea3daed31dd018efb8a2ecd
22051988Di <vitalii.soloninko@gmail.com>
2024-08-26 17:39:07 +0200

    v0.14.3	Checkout. Component.ts part 1

--- frontend/src/app/components/pages/checkout-page/checkout-page.component.ts
+++ frontend/src/app/components/pages/checkout-page/checkout-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { CartService } from '../../../services/cart.service';
+import { UserService } from '../../../services/user.service';
 import { Order } from '../../../shared/models/Order';
 
 @Component({
@@ -8,4 +12,18 @@ import { Order } from '../../../shared/models/Order';
 })
 export class CheckoutPageComponent {
   order: Order = new Order();
+  checkoutForm!: FormGroup;
+
+  constructor(
+    cartService: CartService,
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private toastrService: ToastrService
+  ) {
+    const cart = cartService.getCart();
+    this.order.items = cart.items;
+    this.order.totalPrice = cart.totalPrice;
+  }
+
+  ngOnInit(): void {}
 }
