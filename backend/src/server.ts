4258b799cde60d4a9ea07247dc555f4035572f73
22051988Di <vitalii.soloninko@gmail.com>
2024-09-11 15:14:54 +0200

    v0.21.6	Deploy 6 - Public folder config in server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,12 @@
 import dotenv from 'dotenv'
 dotenv.config()
+import path from 'path'
 
 import cors from 'cors'
 import express from 'express'
+import { dbConnect } from './configs/database.config'
 import foodRouter from './routers/food.router'
 import userRouter from './routers/user.router'
-import { dbConnect } from './configs/database.config'
 dbConnect()
 
 // cors for redirect localhost server
@@ -24,7 +25,12 @@ app.use(
 app.use('/api/foods', foodRouter)
 app.use('/api/users', userRouter)
 
-const port = 5000
+app.use(express.static('public'))
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+const port = process.env.PORT || 5000
 app.listen(port, () => {
 	console.log('Website served on http://localhost: ' + port)
 })
