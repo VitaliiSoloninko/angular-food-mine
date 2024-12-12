ed480ecddf25ce173f70538cc182b89c582b97f8
22051988Di <vitalii.soloninko@gmail.com>
2024-12-12 16:23:38 +0100

    v0.15.2	Checkout. Order items list with products

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { CheckoutPageComponent } from './components/pages/checkout-page/checkout-page.component';
 import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
@@ -17,13 +18,14 @@ import { InputContainerComponent } from './components/partials/input-container/i
 import { InputValidationComponent } from './components/partials/input-validation/input-validation.component';
 import { LoadingComponent } from './components/partials/loading/loading.component';
 import { NotFoundComponent } from './components/partials/not-found/not-found.component';
+import { OrderItemsListComponent } from './components/partials/order-items-list/order-items-list.component';
 import { SearchComponent } from './components/partials/search/search.component';
+import { StarRatingComponent } from './components/partials/star-rating/star-rating.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
 import { TextInputComponent } from './components/partials/text-input/text-input.component';
 import { TitleComponent } from './components/partials/title/title.component';
 import { LoadingInterceptor } from './shared/interceptors/loading.interceptor';
-import { CheckoutPageComponent } from './components/pages/checkout-page/checkout-page.component';
-import { StarRatingComponent } from './components/partials/star-rating/star-rating.component';
+import { OrderItemsComponent } from './components/partials/order-items/order-items.component';
 
 @NgModule({
   declarations: [
@@ -45,6 +47,8 @@ import { StarRatingComponent } from './components/partials/star-rating/star-rati
     LoadingComponent,
     CheckoutPageComponent,
     StarRatingComponent,
+    OrderItemsListComponent,
+    OrderItemsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/partials/order-items-list/order-items-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Order } from '../../../shared/models/Order';
+
+@Component({
+  selector: 'app-order-items-list',
+  standalone: false,
+
+  templateUrl: './order-items-list.component.html',
+  styleUrl: './order-items-list.component.css',
+})
+export class OrderItemsListComponent {
+  @Input()
+  order!: Order;
+}
