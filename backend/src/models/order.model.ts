45f4a018440d8a0f9bcda30590b5e635566442b5
22051988Di <vitalii.soloninko@gmail.com>
2024-12-13 15:53:38 +0100

    v0.16.1	Backend - Order. Model, Schema

---
+++ backend/src/models/order.model.ts
@@ -0,0 +1,35 @@
+import { Schema } from 'mongoose'
+import { Food, FoodSchema } from './food.model'
+
+export interface LatLng {
+	lat: string
+	lng: string
+}
+
+export const LatLngSchema = new Schema<LatLng>({
+	lat: { type: String, required: true },
+	lng: { type: String, required: true },
+})
+
+export interface OrderItem {
+	food: Food
+	price: number
+	quantity: number
+}
+
+export const OrderItemSchema = new Schema<OrderItem>({
+	food: { type: FoodSchema, required: true },
+	price: { type: Number, required: true },
+	quantity: { type: Number, required: true },
+})
+
+export interface Order {
+	id: number
+	items: OrderItem[]
+	totalPrice: number
+	name: string
+	address: string
+	paymentID: string
+	createdAt: string
+	status: string
+}
