b18b120f7a16d8f9ac5ab99f9030d3fa9849e763
22051988Di <vitalii.soloninko@gmail.com>
2024-12-25 14:54:08 +0100

    v0.18.6	Map. Make readonly

--- frontend/src/app/components/partials/map/map.component.ts
+++ frontend/src/app/components/partials/map/map.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  ViewChild,
+} from '@angular/core';
 import {
   icon,
   LatLng,
@@ -21,9 +27,12 @@ import { Order } from '../../../shared/models/Order';
   templateUrl: './map.component.html',
   styleUrl: './map.component.css',
 })
-export class MapComponent {
+export class MapComponent implements OnChanges {
   @Input()
   order!: Order;
+
+  @Input()
+  readonly = false;
 
   private readonly MARKER_ZOOM_LEVEL = 16;
   private readonly MARKER_ICON = icon({
@@ -40,8 +49,28 @@ export class MapComponent {
 
   constructor(private locationService: LocationService) {}
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    if (!this.order) return;
     this.initializeMap();
+
+    if (this.readonly && this.addressLatLng) {
+      this.showLocationOnReadonlyMode();
+    }
+  }
+  showLocationOnReadonlyMode() {
+    const m = this.map;
+    this.setMarker(this.addressLatLng);
+    m.setView(this.addressLatLng, this.MARKER_ZOOM_LEVEL);
+
+    m.dragging.disable();
+    m.touchZoom.disable();
+    m.doubleClickZoom.disable();
+    m.scrollWheelZoom.disable();
+    m.boxZoom.disable();
+    m.keyboard.disable();
+    m.off('click');
+    m.tapHold?.disable();
+    this.currentMarker.dragging?.disable();
   }
 
   initializeMap() {
@@ -86,9 +115,14 @@ export class MapComponent {
 
   // convert latitude and altitude values ​​for mongoDB
   set addressLatLng(latLng: LatLng) {
+    if (!latLng.lat.toFixed) return;
     latLng.lat = parseFloat(latLng.lat.toFixed(8));
     latLng.lng = parseFloat(latLng.lng.toFixed(8));
     this.order.addressLatLng = latLng;
     console.log(this.order.addressLatLng);
+  }
+
+  get addressLatLng() {
+    return this.order.addressLatLng!;
   }
 }
