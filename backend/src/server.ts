6282ec6ca84c8399dc695296a35045d03a84febe
22051988Di <vitalii.soloninko@gmail.com>
2024-12-30 14:42:56 +0100

    v0.21.10	Deploy. v2. angular.json, package.json front, back, root, urls, server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,8 +6,8 @@ import cors from 'cors'
 import express from 'express'
 import { dbConnect } from './configs/database.config'
 import foodRouter from './routers/food.router'
-import userRouter from './routers/user.router'
 import orderRouter from './routers/order.router'
+import userRouter from './routers/user.router'
 dbConnect()
 
 // cors for redirect localhost server
@@ -27,12 +27,12 @@ app.use('/api/foods', foodRouter)
 app.use('/api/users', userRouter)
 app.use('/api/orders', orderRouter)
 
-app.use(express.static('public'))
+app.use(express.static('public/browser'))
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public', 'browser', 'index.html'))
 })
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
-	console.log('Website served on http://localhost:' + port)
+	console.log('Website served on http://localhost: ' + port)
 })
